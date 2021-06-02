1ef16177b50636b56b102cff1e6271d327cee270
Joonas.Masing <joonas.masing@khk.ee>
2021-06-02 16:32:54 +0300

    12 kuud

--- praks6/yl3.sh
+++ praks6/yl3.sh
@@ -1,10 +1,24 @@
+#
+#
 #Sisestades numbri 1-12, kuvatakse kuu nimekirjas vastab kuu
 #Kui number pole 1-12, kuvatakse error, et see ei sobi
-kuu=(null jaanuar veebruar märts aprill mai juuni juuli august september oktoober november detsember)
-echo "Sisesta kuu 1-12:"$input
-read input
-if  [ $input -gt 12 ]; then
-	echo "vale sisend"
-else
-	echo $input" kuu on "${kuu[input]}
-fi
+#Joonas Masing
+#
+
+echo -n "Sisestage kuu number: "
+read kuu
+case $kuu in
+"1") echo "Jaanuar";;
+"2") echo "Veebruar";;
+"3") echo "Marts";;
+"4") echo "Aprill";;
+"5") echo "Mai";;
+"6") echo "Juuni";;
+"7") echo "Juuli";;
+"8") echo "August";;
+"9") echo "September";;
+"10") echo "Oktoober";;
+"11") echo "November";;
+"12") echo "Detsember";;
+*) echo "Sellist kuud ei ole olemas";;
+esac
