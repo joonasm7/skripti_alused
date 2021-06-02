b0d207ec1a9b5320a4ad4e6bebcc01bd201c8739
Joonas.Masing <joonas.masing@khk.ee>
2021-06-02 14:38:37 +0300

    Kasutaja tervitus

--- praks2/yl1.sh
+++ praks2/yl1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-# kasutaja tervitus
+# Kasutaja tervitus 
+# Joonas Masing
 #
-kasutaja_nimi=$(whoami)
-echo -n "Palju õnne, kallis $kasutaja_nimi."
-echo -n "Olete võitnud 1,324,323 euot!"
+user=$(whoami)
+echo -n "Tere, $user"
