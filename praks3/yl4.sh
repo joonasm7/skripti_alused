4a897177e01236a6107d43805b0b805d076b6b27
Joonas.Masing <joonas.masing@khk.ee>
2021-06-02 15:34:27 +0300

    Busside kalkulaator

---
+++ praks3/yl4.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+#Bussiekskursioonide abiline
+#Joonas Masing
+#
+grupp=reisijatearv
+kohad=vaba
+echo "Mitu reisijat on ühes grupis? "
+read reisijad
+echo "Mitu kohta on ühes bussis: "
+read kohad
+
+bussid=$(($reisijad / $kohad))
+ylejaanud=$(($reisijad % $kohad))
+if test $ylejaanud -gt 0
+then
+	bussid=$(($bussid + 1))
+fi
+
+echo "Selleks, et kõik mahuksiud ära, on vaja $bussid bussi" 
