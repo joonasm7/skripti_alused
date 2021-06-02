e61c0c7db4417b215f102e09e717f1184951b50e
Joonas.Masing <joonas.masing@khk.ee>
2021-06-02 14:44:44 +0300

    Nime küsimine ning selle põhjal tervitamine

---
+++ praks3/yl1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+#Skript, mis küsib kasutaja käest tema nime ja tervitab teda selle põhjal
+#Joonas Masing
+#
+echo "Tere. Kirjutage oma ees- ja perekonnanimi: "
+read eesnimi perekonnanimi
+echo "Tere tulemast, $eesnimi $perekonnanimi"
