83f22ae157ff648a2894aa6ecab88a4d27e6ff58
Joonas.Masing <joonas.masing@khk.ee>
2021-06-02 14:53:49 +0300

    4 muutujaga tulemuse kuvamine

---
+++ praks3/yl2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+#4 muutujat ja selle põhjal kombinatsiooni tulemuse väljastamine
+#Joonas Masing
+#
+nimi="Joonas Masing "
+lause1="alustas aastal " 
+aasta1="2020 "
+lause2="ja lõpetab aastal "
+aasta2="2022 "
+kooli="Tartu Kutsehariduskeskuse."
+tulemus="$nimi$lause1$aasta1$lause2$aasta2$kooli"
+echo "$tulemus"
+
